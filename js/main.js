60165e5127274dbf899482f1818d5b08cd5ee87c
alogm <antalogm@gmail.com>
2024-09-05 20:21:33 -0600

    se agregan comentarios al codigo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { calcularTip, calcularTotal, totalAccount, claseActivo } from "./utilidades.js";
 
+// Selecciona elementos del HTML para poder manipular el DOM atraves de #id y .class
 const billInput = document.querySelector("#input-bill");
 const percentageButton = [...document.querySelectorAll(".percentage-button")];
 const peopleInput = document.querySelector("#input-people");
@@ -12,7 +13,13 @@ let tip = 0;
 let bill = 0;
 let people = 1;
 
+// El input de bill detecta el cambio de valor y runPrograma procesa el calculo
+// tip 
 billInput.addEventListener("input", runProgram);
+
+// Se seleccionan todos los botones de los porcentajes, el metodo forEach recorre
+// cada button al detectar el evento click se ejecuta el runProgram pasando 
+// el button como argumento para optener su valor
 percentageButton.forEach(button => {
     if(button.type === "button"){
         button.addEventListener("click", () => runProgram(button));
@@ -21,26 +28,37 @@ percentageButton.forEach(button => {
     }
 });
 
+// addEventListener detecta los cambios que hay en el input 
+// aqui detecta el cambio de personas esto se escribe manual mente
 peopleInput.addEventListener("input", runProgram);
 
+// runProgram se encarga de realizar todos los calculos, 
 function runProgram(button){
-    tip = parseFloat(button.value) || tip;
     bill = parseFloat(billInput.value) || 0;
+    tip = parseFloat(button.value) || tip;
     people = parseFloat(peopleInput.value) || 1;
 
+    // Atravez de innerHTML se actualizan los campos de la interfas html 
+    // de este modo se manipula el DOM 
     tipAmountInput.innerHTML = calcularTip(bill, tip, people);
     totalInput.innerHTML = calcularTotal(bill, tip, people);
     accountInput.innerHTML = totalAccount(bill, tip);
-    console.log(accountInput);
 
-    if (button && button.classList) {  // Verifica si button es un elemento con classList
+    // Comprueba si el estado del button esta activo, el for recorre todos los
+    // buttons de porcentaje y elimina la clase active de cada uno, esto 
+    // asegura que cuando se seleccione un button se deseleccione el anterior 
+    if (button && button.classList) {
         for (let btn of percentageButton) {
             btn.classList.remove("active");
         }
+        // Lama a la funcion claseActivo que añade la clase active, cambiando 
+        // su estilo visual para que indique que esta seleccionado y activo
         claseActivo(button);
     }
 }
 
+// Esta funcion hace que la pagina se recargue con reload al escuchar el evento 
+// click, al escuchar el evento se recarga la pagina y resetea los valores
 resetBtn.addEventListener('click', ()=>{
     location.reload();
 })
--- js/utilidades.js
+++ js/utilidades.js
@@ -1,22 +1,43 @@
-export function reset(billInput, customPercentageBtn, peopleInput, accountInput){
-    billInput = 0,
-    customPercentageBtn = 0,
-    peopleInput = 1, 
-    accountInput = 0
-}
-
+/**
+ * La función calcularTip calcula la cantidad de tip(propina) que cada 
+ * people(persona) debe pagar en función de la cuenta total, el porcentaje 
+ * de propina y el número de personas que compartirán el pago.
+ * @param {*} bill 
+ * @param {*} tip 
+ * @param {*} people 
+ * @returns 
+ */
 export function calcularTip(bill, tip, people){
     return(bill * tip / 100)/people
 }
 
+/**
+ * La función calcularTotal calcula el total del tip + bill, el total se divide 
+ * por el numero total de personas
+ * @param {*} bill 
+ * @param {*} tip 
+ * @param {*} people 
+ * @returns 
+ */
 export function calcularTotal(bill, tip, people){
     return(bill * tip / 100 + bill)/people
 }
 
+/**
+ * La función totalAccount suma el total del tip + bill
+ * @param {*} bill 
+ * @param {*} tip 
+ * @returns 
+ */
 export function totalAccount(bill, tip){
     return(bill * tip / 100 + bill )
 }
 
+/**
+ * 
+ * la función claseActivo sirve para cambiar dinamicamente la apariencia
+ * del input atraves del CSS
+ */
 export function claseActivo(input) {
  input.classList.add("active")
 }
